b3ce71a96b8a07821a1979bfbdff18f3c6e52448
Thomas Bétrancourt <thomas@betrancourt.net>
2013-04-07 10:29:05 +0200

    Gestion de l'entête HTTP 'Content-Type' pour les contenus générés par le plugin

--- app/controllers/redmine_wiki_macros_controller.rb
+++ app/controllers/redmine_wiki_macros_controller.rb
@@ -7,9 +7,15 @@ class RedmineWikiMacrosController < ApplicationController
 		extension = params[:extension]
 		cache_key = self.construct_cache_key_with_macro_name(name, filename, extension)
 		content = Rails.cache.read(cache_key)
+		content_type = 'image/png'
 
+		if(extension == 'svg')
+			content_type = 'image/svg+xml'
+		end
+		
 		if content
-			send_data content, :disposition => 'inline', :filename => name + '_' + filename
+			headers['Content-Type'] = content_type
+			send_data content, :disposition => 'inline', :filename => name + '_' + filename, :type => content_type
 		else
 			render_404
 		end
